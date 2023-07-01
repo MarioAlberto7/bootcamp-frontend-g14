802b1614a40abd2cfc68958c6e4f26931be93511
marioahp <marioahp7.90@gmail.com>
2023-07-01 01:30:21 -0500

    Agregado de header de portafolio con colores personalizados

---
+++ semana05/dia02-aprendiendojs/app.js
@@ -0,0 +1,52 @@
+console.log('Hola Chicos soy el javascript')
+console.log('Hola G14');
+console.log(2+2);
+
+
+// Tipos de datos en javascript
+// Numbers ==> operadores aritmeticos
+
+console.log(100+4);
+console.log(100*4);
+console.log(100/4);
+
+// modulo => residuo
+// 10 / 3 su residuo es 3.333...
+console.log(typeof(10%3));
+
+// string = operador unitario
+console.log(typeof("Hola, mi nombre es Mario"));
+// typeof() me dice que tipo de datos es el valor 
+
+// Booleans
+console.log(typeof(3 == 5));
+console.log(10<15);
+
+// Conversion automatico => coercion de tipo
+
+console.log('5'-1);
+console.log(typeof('5'+1));
+
+// Variables
+
+console.log('Mariana');
+
+
+let age = 18
+
+let nombre
+nombre = 'Gianmarco'
+console.log(nombre);
+
+nombre = 'Cesar'
+console.log(nombre);
+
+nombre = 'Wilson'
+console.log(nombre);
+
+const pais = 'Perú'
+console.log(pais);
+// Ya no puedo utilizar pais 
+// como variable porque con CONST solo guarda una vez
+
+
